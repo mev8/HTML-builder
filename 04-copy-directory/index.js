3efa5332df13934d640f1968f3b0ff3f61844437
Medvediev Yevhen <medvedev.geka@gmail.com>
2023-05-09 00:28:36 +0300

    task 04 complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyDir = async (source, target) => {
+  const sourcePath = path.resolve(source);
+  const targetPath = path.resolve(target);
+
+  await fs.promises.mkdir(targetPath, { recursive: true });
+
+  const sourceFiles = await fs.promises.readdir(sourcePath);
+  for (const sourceFile of sourceFiles) {
+    const sourceFilePath = path.join(sourcePath, sourceFile);
+    const targetFilePath = path.join(targetPath, sourceFile);
+    await fs.promises.copyFile(sourceFilePath, targetFilePath);
+  }
+
+  const targetFiles = await fs.promises.readdir(targetPath);
+  for (const targetFile of targetFiles) {
+    const targetFilePath = path.join(targetPath, targetFile);
+    if (!sourceFiles.includes(targetFile)) {
+      await fs.promises.unlink(targetFilePath);
+    }
+  }
+};
+
+const sourceDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+
+copyDir(sourceDir, destDir)
+  .then(() => console.log('Клонирование успешно!'))
+  .catch((err) => console.error(err));
