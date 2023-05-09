296dc9f3d87d1f2b65c03dc6b749a9e5e3859376
Medvediev Yevhen <medvedev.geka@gmail.com>
2023-05-09 03:05:43 +0300

    task 04 add copy folders

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -11,14 +11,26 @@ const copyDir = async (source, target) => {
   for (const sourceFile of sourceFiles) {
     const sourceFilePath = path.join(sourcePath, sourceFile);
     const targetFilePath = path.join(targetPath, sourceFile);
-    await fs.promises.copyFile(sourceFilePath, targetFilePath);
+
+    const stat = await fs.promises.stat(sourceFilePath);
+    if (stat.isDirectory()) {
+      await copyDir(sourceFilePath, targetFilePath);
+    } else {
+      await fs.promises.copyFile(sourceFilePath, targetFilePath);
+    }
   }
 
   const targetFiles = await fs.promises.readdir(targetPath);
   for (const targetFile of targetFiles) {
     const targetFilePath = path.join(targetPath, targetFile);
+
     if (!sourceFiles.includes(targetFile)) {
-      await fs.promises.unlink(targetFilePath);
+      const stat = await fs.promises.stat(targetFilePath);
+      if (stat.isDirectory()) {
+        await fs.promises.rmdir(targetFilePath, { recursive: true });
+      } else {
+        await fs.promises.unlink(targetFilePath);
+      }
     }
   }
 };
